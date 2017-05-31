dae1bdce8b2ef2fce5fcb7b7a1e49f13b31c1169
Jen Vat <jennyvat@yahoo.com>
2017-05-31 14:13:22 -0600

    fixed alert

--- academyAward.java
+++ academyAward.java
@@ -6,26 +6,11 @@ import java.util.Map; //handles duplicates
 import java.util.Arrays;
 
 
-// public class Award{
-//   private int year;
-//   private String actress;
-//   private String movie;
-//
-//   public Award(int year, String actress, String movie) {
-//     this.year = year;
-//     this.actress = actress;
-//     this.movie = movie;
-//   }
-// }
-
-
 public class academyAward {
 
-  public academyAward(int year, String actress, String movie) {
-      this.year = year;
-      this.actress = actress;
-      this.movie = movie;
+  public static void main(String[] args) {
 
+  //read the file
    String csvFile = "";
    try {
      csvFile = args[0];
@@ -36,46 +21,25 @@ public class academyAward {
 
    String line = "";
    String csvSplitBy = ",";
-  //  HashMap newObj = new HashMap();
-   Map < String, String > newObj = new HashMap < String, String > ();
+   HashMap newObj = new HashMap();
+  //  Map < String, String > newObj = new HashMap < String, String > ();
 
 
    try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-    //   while ((line = br.readLine()) != null) {
-    //     String [] data = line.split(csvSplitBy);
-    //
-    // //  Arrays myArr = new Arrays();
-    //
-    //     newObj.put("Year", data[1]);
-    //     newObj.put("Actress", data[1]);
-    //     newObj.put("Movie", data[2]);
+      while ((line = br.readLine()) != null) {
+        String [] data = line.split(csvSplitBy);
 
-        // Arrays.push(newObj);
-        Pattern pattern = Pattern.compile(",");
+        newObj.put("Year", data[1]);
+        newObj.put("Actress", data[1]);
+        newObj.put("Movie", data[2]);
 
-        List<academyAward> academyAwards = in 
-          .lines()
-          .skip(1)
-          .map(line -> {
-            String[] x = pattern.split(line);
-            return new academyAward(Integer.parseInt(x[0]), x[1], x[2]);
-          })
-
-          .collect(Collectors.toList());
-
-          ObjectMapper mapper = new ObjectMapper();
-          mapper.enable(SerializationFeature.INDENT_OUTPUT);
-          mapper.writeValue(System.out, academyAwards);
-
-
-        // System.out.println(newObj);
-        // System.out.println(data[1]);
+        System.out.println(newObj);
+        System.out.println(data[1]);
 
 
 
-      // } //end of while
-      // Arrays.toString(newObj);
-      // System.out.println(newObj);
+      } //end of while
+      System.out.println(newObj);
 
 
    } catch (IOException e) {
@@ -85,3 +49,23 @@ public class academyAward {
 
  } //end main
 } //end class
+
+
+
+
+// Arrays.push(newObj);
+// Pattern pattern = Pattern.compile(",");
+//
+// List<academyAward> academyAwards = in
+//   .lines()
+//   .skip(1)
+//   .map(line -> {
+//     String[] x = pattern.split(line);
+//     return new academyAward(Integer.parseInt(x[0]), x[1], x[2]);
+//   })
+//
+//   .collect(Collectors.toList());
+//
+//   ObjectMapper mapper = new ObjectMapper();
+//   mapper.enable(SerializationFeature.INDENT_OUTPUT);
+//   mapper.writeValue(System.out, academyAwards);
--- script.js
+++ script.js
@@ -1,28 +1,18 @@
 $(document).ready(function() {
   $.getJSON('http://127.0.0.1:8000/academyAward.json', function(arr){
-    // console.log(arr);
-      for(var i = 0; i < arr.length; i++){  //47 or arr.length
-        // console.log(arr[i].year);
-        // console.log(arr[i].actress);
-        // console.log(arr[i].movie);
+      for(var i = 0; i < arr.length; i++){
         var year = arr[i].year;
         var actress = arr[i].actress;
         var movie = arr[i].movie;
-
         $('#awards tbody').append('<tr class="table"><td>' + year + '</td> <td>' +actress + '</td> <td>' + movie + '</td></tr>')
-
       } //end for loop
   });
 
-  $('tr.table').click(function() {
+  $('#awards').on('click', 'tr.table', function() {
     var tableData = $(this).children("td").map(function() {
         return $(this).text();
     }).get();
+    alert($.trim(tableData[0]) + " • " + $.trim(tableData[1]) + " • " + $.trim(tableData[2]));
+  });
 
-    alert("Your data is: " + $.trim(tableData[0]) + " , " + $.trim(tableData[1]) + " , " + $.trim(tableData[2]));
-    // console.log("clicked");
-    // alert("You!");
-  })
-
-
-})
+});
